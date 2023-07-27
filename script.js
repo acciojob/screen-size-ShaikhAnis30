062e0ee0e6da8faff0a4845369e67e6bd67027ae
ShaikhAnis30 <anisshaikh8535@gmail.com>
2023-07-27 11:21:28 +0530

    Added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function displayChange() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+
+    const h1 = document.getElementById("heightAndWidth");
+    h1.innerText = `Width: ${width} and Height: ${height}`;
+}
+
+displayChange();
+
+window.addEventListener("resize", displayChange);
+
